6bb12c38aaeb7a39ab2aaaf748c0157de467a578
min8grace <56316476+min8grace@users.noreply.github.com>
2021-01-29 03:34:57 -0800

    Feature_Claim_Modified for adding the mapping profie and changing filenames

---
+++ StoreManager.Application/Mappings/ClaimProfile.cs
@@ -0,0 +1,19 @@
+﻿using StoreManager.Application.Features.Claims.Commands.Create;
+using StoreManager.Application.Features.Claims.Queries.GetAllCached;
+using StoreManager.Application.Features.Claims.Queries.GetById;
+using StoreManager.Domain.Entities.Catalog;
+using AutoMapper;
+using StoreManager.Domain.Entities.Expense;
+
+namespace StoreManager.Application.Mappings
+{
+    internal class ClaimProfile : Profile
+    {
+        public ClaimProfile()
+        {
+            CreateMap<CreateClaimCommand, Claim>().ReverseMap();
+            CreateMap<GetClaimByIdResponse, Claim>().ReverseMap();
+            CreateMap<GetAllClaimsCachedResponse, Claim>().ReverseMap();
+        }
+    }
+}
